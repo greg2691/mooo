29f0e54a312576f59478077c805427c415e72c50
greg2691 <condellogregory@icloud.com>
2024-05-10 13:33:49 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Moji-Moji`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
